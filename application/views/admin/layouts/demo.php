ac32d6fafac7c723315989f01cfdc3f8aef4808d
Khorshed Alam <andgatetech@gmail.com>
2017-08-23 21:00:55 +0600

    Basic configuration setup

---
+++ application/views/admin/layouts/demo.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-lg-12 col-xs-12 col-sm-12">
+        <div class="portlet light portlet-fit bordered">
+            Section A
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-xs-12 col-sm-12">
+        <div class="portlet light portlet-fit bordered">
+            Section C
+        </div>
+    </div>
+    <div class="col-lg-6 col-xs-12 col-sm-12">
+        <div class="portlet light portlet-fit bordered">
+            Section D
+        </div>
+    </div>
+</div>
